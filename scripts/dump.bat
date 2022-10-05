1c2e1254315a75a48587704b87e03499f911dcf7
wtain <ramiz.zeynalov@gmail.com>
2022-10-06 01:31:20 +0200

    Comments mighration scripts and queries

--- scripts/dump.bat
+++ scripts/dump.bat
@@ -1,1 +1,2 @@
 mongoexport --collection=bookmarks mongodb://localhost:27018/bookmarks_store --out=bookmarks.json
+mongoexport --collection=comments mongodb://localhost:27018/bookmarks_store --out=comments.json
--- scripts/restore.bat
+++ scripts/restore.bat
@@ -1,1 +1,2 @@
 mongoimport --collection=bookmarks mongodb://localhost:27018/bookmarks_store bookmarks.json
+mongoimport --collection=comments mongodb://localhost:27018/bookmarks_store comments.json
